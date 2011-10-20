c3c1a33d3d8785b9fd16bd2800c19a5b960fdaf5
Alexey Filippov <afilippov@acm.org>
2011-10-20 22:25:41 +0100

    A nice example on decimals

--- src/org/acm/afilippov/stacko/Doubles.java
+++ src/org/acm/afilippov/stacko/Doubles.java
@@ -1,7 +1,15 @@
 package org.acm.afilippov.stacko;
 
+import java.math.BigDecimal;
+
 public class Doubles {
     public static void main(String[] args) {
+        sum();
+
+        System.out.println(new BigDecimal(2.32));
+    }
+
+    private static void sum() {
         double v = 0;
         for (int i = 0; i < 100; i++)
             v += 0.01;
